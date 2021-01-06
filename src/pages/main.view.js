088d6304fc911c4a7f53b4fa0b2c5652d83877c0
MarcosIraola <iraolamarcos@gmail.com>
2021-01-06 12:51:30 -0300

    cambios

--- src/pages/main.view.js
+++ src/pages/main.view.js
@@ -21,9 +21,11 @@ const Main = () => {
                     </p>
                 </div>
 
+                <div className={styles.parrafoConFoto}>
                 <p className={styles.parrafo}>
                     Many years ago – I don’t remember exactly how many- I guided three men. They had a clear purpose and I had mine as well: to become their guide. These men had different ways to connect with me and receive my messages. The first one was a rich and recognized astronomer. He followed my movements through his cartographic skills. He was a great observer of the sky. The second one reached out from the bottom of his soul. Wondering in his inner world, he found the way to see my signals in the outer one.  And the third one, the most innocent one, was a man of intuition. Deep inside, he knew his journey had a purpose. He travelled without knowing where he would end up, letting himself be carried away by his heart, convinced that there was something invisible, a kind of magnet that took him to the places where he had to be.
                 </p>
+                </div>
 
                 <div className={styles.parrafoConFoto}>
                     <p className={styles.parrafoConFoto}>
@@ -34,34 +36,43 @@ const Main = () => {
                     <img className={styles.imgStar} src={foto}/>
                 </div>
 
-                <p className={styles.parrafo}>
-                    -Again? she asked while kissing his forehead.
-                </p>
+                <div className={styles.parrafoConFoto}>
+                    <p className={styles.parrafo}>
+                        -Again? she asked while kissing his forehead.
+                    </p>
+                </div>
 
-                <p className={styles.parrafo}>
-                    After a while, he came out with his body covered with towels. His white clothes hung ready as if they were actually waiting for him. He looked at the gold embroidery on the sleeves and remembered the clothes of his childhood. Pounding memories of the day his mother died came to his mind. Gaspar was playing when his father, broken in tears, hugged him. He told him that his mother and brother had not survived the procedure, but deep down he already knew.
-                    <br/>
-                    After dressing himself up, he decided to walk around the palace. The place was beautiful: a large and luminous building with the floor covered with colorful carpets and surrounded by gardens full of animals and smells. He looked at every detail around him and felt grateful for all the peaceful years spent with his wife and all the people that looked out for their welfare. He hugged his wife for a while in silence and then continued walking towards the room where he used to work on his documents. On the walls of his private room, there were huge drawings where he made traces connecting dots from the sky to Earth. And on the big central table there were giant charts and maps that Gaspar designed every morning and night of his life. He knew all about constellations, but his real passion was the moon: powerful and mysterious.
-                    <br/>
-                    I remember that special morning when he unfolded his most complete cartographic drawing. He translated the information he had visualized on the map and everything made sense. The point in the center of the triangle became a real location: an area to the west, more precisely, what today humans call the Zagros Mountains. Through calculations and with a little bit of intuition, he worked on the position that the stars would have during the indicated date. He counted the sunrises and concluded that it would take him almost a month to reach that location. He did not believe in coincidences and this is why he smiled with complicity when he realized that a great planetary conjunction was about to happen that given day.
-                </p>
+                <div className={styles.parrafoConFoto}>
+                    <p className={styles.parrafo}>
+                        After a while, he came out with his body covered with towels. His white clothes hung ready as if they were actually waiting for him. He looked at the gold embroidery on the sleeves and remembered the clothes of his childhood. Pounding memories of the day his mother died came to his mind. Gaspar was playing when his father, broken in tears, hugged him. He told him that his mother and brother had not survived the procedure, but deep down he already knew.
+                        <br/>
+                        After dressing himself up, he decided to walk around the palace. The place was beautiful: a large and luminous building with the floor covered with colorful carpets and surrounded by gardens full of animals and smells. He looked at every detail around him and felt grateful for all the peaceful years spent with his wife and all the people that looked out for their welfare. He hugged his wife for a while in silence and then continued walking towards the room where he used to work on his documents. On the walls of his private room, there were huge drawings where he made traces connecting dots from the sky to Earth. And on the big central table there were giant charts and maps that Gaspar designed every morning and night of his life. He knew all about constellations, but his real passion was the moon: powerful and mysterious.
+                        <br/>
+                        I remember that special morning when he unfolded his most complete cartographic drawing. He translated the information he had visualized on the map and everything made sense. The point in the center of the triangle became a real location: an area to the west, more precisely, what today humans call the Zagros Mountains. Through calculations and with a little bit of intuition, he worked on the position that the stars would have during the indicated date. He counted the sunrises and concluded that it would take him almost a month to reach that location. He did not believe in coincidences and this is why he smiled with complicity when he realized that a great planetary conjunction was about to happen that given day.
+                    </p>
+                </div>
 
                 <img className={styles.img} src={fondo}/>
 
-                <p className={styles.separador}>
-                    He took some days to double-check his calculations and to make a small travel version of his map. He also went out to the village to buy the best camel he could find and prepared all the elements he would travel with: containers for water, clothes and gold.
-                    <br/>
-                    When the day of his departure arrived, he kissed his wife, and she hugged him back with enthusiasm. For the first time, Gaspar looked hesitant. He was afraid to leave her alone and not ever return back home. But she was calm and loving. She looked him in the eye and dropped a few words in his ear. He smiled, reached out to his pocket, and found a piece of paper. She didn’t let him read it. Not yet, she said. And with a smile on his face, Gaspar got on his camel and greeted all those who had come to wish him luck.
-                    <br/>
-                    Two weeks later, almost halfway there, he stopped by at a small house in the middle of nowhere. He was well received by an old man, who lived by himself. When Gaspar walked into the house, his eyes widened in surprise. On the walls he found drawings like the ones he made.
-                    <br/>
-                    The old man saw the rounded eyebrows on Gaspar’s face and immediately recognized that he was facing a man who understood him.
-                </p>
+                <div className={styles.parrafoConFoto}>
+                    <p className={styles.separador}>
+                        He took some days to double-check his calculations and to make a small travel version of his map. He also went out to the village to buy the best camel he could find and prepared all the elements he would travel with: containers for water, clothes and gold.
+                        <br/>
+                        When the day of his departure arrived, he kissed his wife, and she hugged him back with enthusiasm. For the first time, Gaspar looked hesitant. He was afraid to leave her alone and not ever return back home. But she was calm and loving. She looked him in the eye and dropped a few words in his ear. He smiled, reached out to his pocket, and found a piece of paper. She didn’t let him read it. Not yet, she said. And with a smile on his face, Gaspar got on his camel and greeted all those who had come to wish him luck.
+                        <br/>
+                        Two weeks later, almost halfway there, he stopped by at a small house in the middle of nowhere. He was well received by an old man, who lived by himself. When Gaspar walked into the house, his eyes widened in surprise. On the walls he found drawings like the ones he made.
+                        <br/>
+                        The old man saw the rounded eyebrows on Gaspar’s face and immediately recognized that he was facing a man who understood him.
+                    </p>
+                </div>
+                
+                <div className={styles.parrafoConFoto}>
+                    <p className={styles.parrafo}>
+                        -You are also an astronomer, right? he asked.
+                    </p>
+                </div>
 
-                <p className={styles.parrafo}>
-                    -You are also an astronomer, right? he asked.
-                </p>
-
+                <div className={styles.parrafoConFoto}>
                 <p className={styles.parrafo}>
                     There was no need to answer that question. They connected with each other right away. They spent the whole night talking, pooling everything they knew. And they laughed and got excited when they went outside to watch the moon. It got late and the man offered Gaspar to spend the night there, and he accepted.
                     <br/>
@@ -69,15 +80,21 @@ const Main = () => {
                     <br/>
                     The man, full of curiosity, asked if he could take a look at his map. Gaspar -proud and confident- spread the document over the table, waiting for the approval of an old astronomer. However, he began to laugh.
                 </p>
+                </div>
                 
+                <div className={styles.parrafoConFoto}>
                 <p className={styles.parrafo}>
                     - What are you talking about? You are crazy. There is no such star. All these calculations are so inaccurate!
                 </p>
+                </div>
 
+                <div className={styles.parrafoConFoto}>
                 <p className={styles.parrafo}>
                     Gaspar felt betrayed and disappointed by the old man who kept laughing and making fun of him. But he tried not to react and left the house without saying a word, confident of his calculations and my guide. He spent the next days following the merchant’s trading route and he could see, from afar, the peak of the mountains. The meeting point was nearby.
                 </p>
+                </div>
 
+                <div className={styles.parrafoConFoto}>
                 <p className={styles.parrafo}>
                     The second one, Balthazar, was also a great astronomer but before all else, he was a healer. He had learned about the cycles of life walking through the desert along with his grandfather, observing animals and plants and paying attention to Life. He spent his days in nature, playing with stones and crystals, looking for the hidden water in the heart of desert flowers. He created his own recipes. In his hands he carried consciously the gift of healing: he predicted the reactions of the body. He was always smiling, wondering between that silent gap of his body and his thought, where he could find all the answers.
                     <br/>
@@ -85,24 +102,33 @@ const Main = () => {
                     <br/>
                     One day, he heard my voice and recognized me right away. I was short and clear. I showed him the meeting point. I told him about the journey that Gaspar was making. I explained the story of Melchior. And I showed him on a map the best way to get to Bethlehem. It was when I told him they had to visit a newborn that Balthazar broke his silence:
                 </p>
+                </div>
 
+                <div className={styles.parrafoConFoto}>
                 <p className={styles.parrafo}>
                     - Why me?
                 </p>
+                </div>
 
+                <div className={styles.parrafoConFoto}>
                 <p className={styles.parrafo}>
                     - Because it is your purpose.
                 </p>
+                </div>
 
+                <div className={styles.parrafoConFoto}>
                 <p className={styles.parrafo}>
                     Not long after that, he left. The meeting point was waiting for him and it wasn’t far away from his home.
                 </p>
+                </div>
 
+                <div className={styles.parrafoConFoto}>
                 <p className={styles.parrafo}>
                     I guided Melchior through the so-called coincidences. He was a strong man born in Greece, close to Ephesus. It had been a long time since he had lost his family and his land due to an invasion. He was the only family member who survived and managed to escape. He travelled in search of something that he did not quite know what it was. He knew he had a mission to accomplish and he payed attention to signs and coincidences. Without recognizing me, he knew me and trusted me because I was nothing more than his own intuition. 
                     <br/>
                     These three wise men taught me how to communicate with people on Earth. So many times, so many, I heard people claim that I was just a shooting star, someone who vanished without a trace. Others called me the Star of Bethlehem. And the reason why I never felt the need to say my real name is because I have always believed that my name is not important, what really matters is the secret that lives in the story I am about to tell…
                 </p>
+                </div>
             </div>
 
         </div>
